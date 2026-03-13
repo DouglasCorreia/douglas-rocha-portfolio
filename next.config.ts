e7c4d56a079a6a95772048a47db0c3791c0d5b9b
Douglas R <douglascorreia17@gmail.com>
2026-03-13 12:47:14 -0300

    fix: Corrigido o next.config.ts por conta de erro do i18n não suportado no app router.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  i18n: {
-    locales: ["pt-BR", "en"],
-    defaultLocale: "pt-BR"
-  }
+
 };
 
 export default nextConfig;
